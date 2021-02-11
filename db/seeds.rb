b0e52392b8aef073066cad7073bcdafc9f2ac326
sean <maherseanmilo@gmail.com>
2021-02-10 18:52:33 -0600

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,8 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Place.create(name: "Old Apartment", address: "4610 N Harding Ave")
+Place.create(name: "New Apartment", address: "2726 N Mozart St")
+Place.create(name: "Ohio Apartment", address: "325 E Maple St")
+Place.create(name: "Childhood Home", address: "354 N Granger St")
